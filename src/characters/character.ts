bfab15c9e7c224bb3ff50bd1cab7e35fed6044a8
tomekDom <thron@knights.pl>
2017-07-01 00:47:55 +0200

    #master  * changed blood

--- src/characters/character.ts
+++ src/characters/character.ts
@@ -183,23 +183,23 @@ abstract class Character {
         particleSystem.maxEmitBox = new BABYLON.Vector3(0, this.mesh.geometry.extend.maximum.y*0.7, 0); // To...
 
         particleSystem.color1 = new BABYLON.Color4(1, 0, 0, 1);
-        particleSystem.color2 = new BABYLON.Color4(1, 0, 0, 1);
-        particleSystem.colorDead = new BABYLON.Color4(1, 0, 0, 0.0);
+        particleSystem.color2 = new BABYLON.Color4(1, 0, 0, 0.1);
+        particleSystem.colorDead = new BABYLON.Color4(0, 0, 0, 1);
 
-        particleSystem.minSize = 0.1;
-        particleSystem.maxSize = 0.2;
+        particleSystem.minSize = 0.3;
+        particleSystem.maxSize = 0.5;
 
         particleSystem.minLifeTime = 0.05;
         particleSystem.maxLifeTime = 0.7;
 
         particleSystem.emitRate = 500;
 
-        //particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+        particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
 
         particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0);
 
-        particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
-        particleSystem.direction2 = new BABYLON.Vector3(0, 5, -4);
+        particleSystem.direction1 = new BABYLON.Vector3(-2, 2, -2);
+        particleSystem.direction2 = new BABYLON.Vector3(2, 2, 2);
         particleSystem.targetStopDuration = 0.6;
         particleSystem.minAngularSpeed = -10.0;
         particleSystem.maxAngularSpeed = 10.0;
